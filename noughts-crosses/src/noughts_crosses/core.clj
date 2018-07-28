add6c912f2ab9c6d5e6473875c4c67f6f96016b4
siwally <simon.wallington@ovoenergy.com>
2018-07-28 16:26:09 +0100

    Detect winner in any row.

--- noughts-crosses/src/noughts_crosses/core.clj
+++ noughts-crosses/src/noughts_crosses/core.clj
@@ -3,16 +3,36 @@
 
 (defn initial-grid
   []
-  [:- :- :-])
+  [:- :- :-   ; 0, 1, 2
+   :- :- :-   ; 3, 4, 5
+   :- :- :-]) ; 6, 7, 8
+
+   ; verticals - just need same mod for three els
+   ; diagonals from l-to-r, for each row: (row* 3) + row
+   ; diagonals from r-to-l: (3 * row+1) - 1, then - row
 
 (defn move
   [grid pos plyr]
-  {:pre [(<= 0 pos 2), (= :- (nth grid pos))]}
-    (assoc grid pos plyr))
+  {:pre [(<= 0 pos (dec (count grid))), (= :- (nth grid pos))]}
+  (assoc grid pos plyr))
+
+(defn idxs-in-row
+  [row]
+  (range (* row 3) (* (inc row) 3)))
+
+;  (map vector (range 3)  [:X :X :X])
+; make map with the index, then filter based on the keys - better than nth?
+
+; can turn into winning-line and take indexes or fn, but build scenarios first
+(defn winning-row?
+  [grid row plyr]()
+  (let [moves (map #(nth grid %1) (idxs-in-row row))]
+  (= (count moves) (count (filter #(= %1 plyr) moves)))
+))
 
 (defn winner?
   [grid plyr]
-  (= (count grid) (count (filter #(= % plyr) grid))))
+  (or (winning-row? grid 0 plyr) (winning-row? grid 1 plyr) (winning-row? grid 2 plyr)))
 
 (defn play-fn
   []
--- noughts-crosses/test/noughts_crosses/core_test.clj
+++ noughts-crosses/test/noughts_crosses/core_test.clj
@@ -9,7 +9,6 @@
               (move 0 :X)
               (move 1 :X)
               (move 2 :X))]
-      (is (= [:X :X :X] grid))
       (is (true? (winner? grid :X)))
       (is (false? (winner? grid :O))))))
 
@@ -18,4 +17,4 @@
     (let [f (play-fn) grid (do (f 0 :X) (f 1 :X) (f 2 :O))]
       (is (thrown? AssertionError (move grid 2 :X)))
       (is (thrown? AssertionError (move grid -1 :X)))
-      (is (thrown? AssertionError (move grid 3 :X))))))
+      (is (thrown? AssertionError (move grid 9 :X))))))
