9fb4ba239b1403a4e33fe535e8a2c63532716ffa
siwally <simon.wallington@ovoenergy.com>
2018-07-29 12:58:44 +0100

    Add support for diagonal wins, from left to right.

--- noughts-crosses/src/noughts_crosses/core.clj
+++ noughts-crosses/src/noughts_crosses/core.clj
@@ -1,37 +1,50 @@
 (ns noughts-crosses.core
   (:gen-class))
 
+(def grid-width  3)
+(def grid-el-count (* grid-width grid-width))
+
 (defn initial-grid
   []
   [:- :- :-   ; 0, 1, 2
    :- :- :-   ; 3, 4, 5
    :- :- :-]) ; 6, 7, 8
 
-   ; diagonals from l-to-r, for each row: (row* 3) + row
-   ; diagonals from r-to-l: (3 * row+1) - 1, then - row
+   ; TODO diagonal from r-to-l: (3 * row+1) - 1, then - row
 
 (defn move
   [grid pos plyr]
-  {:pre [(< -1 pos (count grid)), (= :- (nth grid pos))]}
+  {:pre [(< -1 pos grid-el-count), (= :- (nth grid pos))]}
   (assoc grid pos plyr))
 
+; Can simplify with reduce or by creating map with idx and keys up front?
 (defn winning-line?
-  [get-idxs-fn grid plyr]
-  (let [moves (map #(nth grid %1) (get-idxs-fn))] ; TODO Simplify, maybe reducing by row or col key?
+  [fn-idxs-for-line grid plyr]
+  (let [moves (map #(nth grid %1) (fn-idxs-for-line))]
     (= (count moves) (count (filter #(= %1 plyr) moves)))))
 
 (defn winning-row?
   [grid row plyr]
-  (winning-line? (fn [] (range (* row 3) (* (inc row) 3))) grid plyr))
+  (winning-line? #(range (* row grid-width) (* (inc row) grid-width))
+                 grid
+                 plyr))
 
 (defn winning-col?
   [grid col plyr]
-  (winning-line? (fn [] (filter (fn [i] (= col (mod i 3))) (range 9))) grid plyr))
+  (winning-line? #(filter (fn [i] (= col (mod i grid-width))) (range grid-el-count))
+                 grid
+                 plyr))
 
+(defn winning-diag?
+  [grid plyr]
+  (winning-line? (fn [] (map #(+ % (* % grid-width)) (range grid-width)))
+                 grid
+                 plyr))
 (defn winner?
   [grid plyr]
   (or (winning-row? grid 0 plyr) (winning-row? grid 1 plyr) (winning-row? grid 2 plyr)
-      (winning-col? grid 0 plyr) (winning-col? grid 1 plyr) (winning-col? grid 2 plyr)))
+      (winning-col? grid 0 plyr) (winning-col? grid 1 plyr) (winning-col? grid 2 plyr)
+      (winning-diag? grid plyr)))
 
 (defn play-fn
   []
