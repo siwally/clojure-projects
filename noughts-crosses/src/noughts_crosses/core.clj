56a4208bc1721e4e78cffdbeea6d9fcebbeb8886
siwally <simon.wallington@ovoenergy.com>
2018-07-30 16:33:34 +0100

    Add basic interactive gameplay, albeit with messy printing.

--- noughts-crosses/src/noughts_crosses/core.clj
+++ noughts-crosses/src/noughts_crosses/core.clj
@@ -15,49 +15,50 @@
   {:pre [(< -1 pos grid-el-count), (= :- (nth grid pos))]}
   (assoc grid pos plyr))
 
-; Can simplify with reduce or by creating map with idx and keys up front?
+; Simplify by creating map with idx and keys first, or collecting into groups?
 (defn winning-line?
-  [fn-idxs-for-line grid plyr]
-  (let [moves (map #(nth grid %1) (fn-idxs-for-line))]
+  [fn-idxs grid plyr]
+  (let [moves (map #(nth grid %1) (fn-idxs))]
     (= grid-width (count (filter #(= %1 plyr) moves)))))
 
-; Make winning-row? and winning-col check all rows and columns, using grid-width
+; Could form a closure over row and call multiple times from here?
 (defn winning-row?
   [grid row plyr]
-  (winning-line? #(range (* row grid-width) (* (inc row) grid-width))
-                 grid
-                 plyr))
+  (let [fn-idxs #(range (* row grid-width) (* (inc row) grid-width))]
+  (winning-line? fn-idxs grid plyr)))
 
 (defn winning-col?
   [grid col plyr]
-  (winning-line? #(filter (fn [i] (= col (mod i grid-width))) (range grid-el-count))
-                 grid
-                 plyr))
+  (let [fn-idxs #(filter (fn [i] (= col (mod i grid-width))) (range grid-el-count))]
+  (winning-line? fn-idxs grid plyr)))
 
 (defn winning-diag?
   [grid plyr]
-  (or
-   ; pass indexes for diagonal line, starting at top-left corner
-   (winning-line? (fn [] (map #(+ % (* % grid-width)) (range grid-width)))
-                  grid
-                  plyr)
-   ; pass indexes for diagonal line, starting at top-right corner
-   (winning-line? (fn [] (map #(+ (- (dec grid-width) %) (* % grid-width)) (range grid-width)))
-                  grid
-                  plyr)))
+  (let [fn-idxs-top-l (fn [] (map #(+ % (* % grid-width)) (range grid-width)))
+        fn-idxs-top-r (fn [] (map #(+ (- (dec grid-width) %) (* % grid-width)) (range grid-width)))]
+  (or (winning-line? fn-idxs-top-l grid plyr) (winning-line? fn-idxs-top-r grid plyr))))
 
 (defn winner?
   [grid plyr]
-  (or (winning-row? grid 0 plyr) (winning-row? grid 1 plyr) (winning-row? grid 2 plyr)
-      (winning-col? grid 0 plyr) (winning-col? grid 1 plyr) (winning-col? grid 2 plyr)
-      (winning-diag? grid plyr)))
+  (let [winning-rows (map #(winning-row? grid % plyr) (range 0 grid-width))
+        winning-cols (map #(winning-col? grid % plyr) (range 0 grid-width))]
+    (or (some true? winning-rows)
+        (some true? winning-cols)
+        (winning-diag? grid plyr))))
 
-; TODO Handle invalid input and allow retry
+(defn prn-grid
+  ([grid] (prn-grid grid 0))
+  ([grid idx]
+    (when (= 0 (mod idx grid-width)) (newline)) ; new row
+    (print (format "(%d)%s " idx (nth grid idx)))
+    (when (< idx (dec grid-el-count)) (recur grid (inc idx)))))
+
+; TODO General clean-up (split fns?), plus handle invalid input and allow retry.
+; TODO Handle all positions taken but no winner, and stop recurring.
 (defn play
   [grid plyr]
-  (prn grid)
-  (prn (format "Your move, %s" plyr))
+  (do (prn-grid grid) (newline) (newline) (println (format "Your move, %s" plyr)))
   (let [new-grid (move grid (Integer/parseInt (read-line)) plyr)]
     (if (true? (winner? new-grid plyr))
-        (prn (format "%s wins!" plyr))
+        (println (format "%s wins!" plyr))
         (recur new-grid (if (= :X plyr) :O :X)))))
