0a1312dc013a5b708b799e923ea00f529a4e6b5d
Martino Visintin <martino.visintin@ovoenergy.com>
2018-08-07 14:18:10 +0100

    alternate solution

--- noughts-crosses/src/noughts_crosses/game.clj
+++ noughts-crosses/src/noughts_crosses/game.clj
@@ -79,5 +79,164 @@
       (winning-col? grid plyr)
       (winning-diag? grid plyr)))
 
-; TODO Potentially simplify winning-col, either with partioning or group-by for col mod
-; Something like (map #(map first (partition-all grid-width (range % grid-el-count))) (range grid-width))?
+;; TODO Potentially simplify winning-col, either with partioning or group-by for col mod
+;; Something like (map #(map first (partition-all grid-width (range % grid-el-count))) (range grid-width))?
+
+;; ^^^^^^^^^^^^^^ .. i'm not sure i love the passing around of indexes.. they couple
+;; these fns to the concrete implementation of the grid and they make a lot of
+;; the small utilities non-reusable. i'd rather work with the concepts of rows,
+;; cols, and diagonals.
+
+;; i'd start by defining your "accessors": rows, cols and diagonals
+
+(defn grid-rows
+  [grid-width grid]
+  (partition grid-width grid))
+
+(defn grid-cols
+  [grid-width grid]
+  (->> grid
+       ;; i wouldn't worry about efficiency at all here.. there's always time to
+       ;; optimize later
+       (grid-rows grid-width)
+       (apply map vector)))
+
+(grid-rows 2 [:a :b
+              :c :d])
+;; => ((:a :b) (:c :d))
+
+(grid-cols 3 [:a :b :c
+              :d :e :f
+              :g :h :i])
+
+;; as I was about to define diagonals, the issue of non-square boards arised. so
+;; now we get an extra fn:
+(defn grid-square?
+  [grid-width grid]
+  (= (count (grid-rows grid-width grid))
+     (count (grid-cols grid-width grid))))
+
+(grid-square? 2 [:a :a
+                 :a :a])
+;; => true
+
+(grid-square? 2 [:a :b
+                 :c :d
+                 :e :f])
+;; => false
+
+(defn grid-diags
+  [grid-width grid]
+    (when (grid-square? grid-width grid)
+      (let [rows               (grid-rows grid-width grid)
+            diag-from-top-left (fn [rows]
+                                 (->> rows
+                                      (map (fn [idx row] (nth row idx))
+                                           (range))))]
+        [(diag-from-top-left rows)
+         (diag-from-top-left (map reverse rows))])))
+
+(grid-diags 3 [:a :b :c
+               :d :e :f
+               :g :h :i])
+;; => [(:a :e :i) (:c :e :g)]
+
+;; non-square grid doesn't have a diagonal
+(grid-diags 2 [:a :b
+               :c :d
+               :e :f])
+;; => nil
+
+;; just some utils for what's coming
+
+(defn single-element? [coll]
+  (= 1 (count coll)))
+
+(defn every= [coll]
+  (->> coll set single-element?))
+
+(every= [:a :b])
+;; => false
+(every= [:a :a])
+;; => true
+
+(defn line-full?
+  [line]
+  (not-any? (partial = :-) line))
+
+;; Once your accessors are done, we can define a generic line-winner
+(defn line-winner
+  [line]
+  (when (and (line-full? line)
+             (every= line))
+    (first line)))
+
+(line-winner [:X :X :X])
+;; => :X
+(line-winner [:X :O :X])
+;; => nil
+(line-winner [:- :- :-])
+;; => nil
+
+;; ... and we define a grid-winner in those terms
+(defn grid-winner
+  [grid-width grid]
+  (let [rows    (grid-rows grid-width grid)
+        cols    (grid-cols grid-width grid)
+        diags   (grid-diags grid-width grid)
+        winners (->> (concat (map line-winner rows)
+                             (map line-winner cols)
+                             ;; note map over nil is just nil so we don't need
+                             ;; to worry
+                             (map line-winner diags))
+                     set
+                     (remove nil?))]
+    (when (single-element? winners)
+      (first winners))))
+
+;; non-square grid
+(grid-winner 2 [:a :b
+                :c :d
+                :e :f])
+;; => nil
+
+;; no win
+(grid-winner 2 [:a :b
+                :c :d])
+;; => nil
+
+;; row win
+(grid-winner 2 [:a :a
+                :c :d])
+;; => :a
+
+;; col win
+(grid-winner 2 [:a :b
+                :a :d])
+;; => :a
+
+;; diag win
+(grid-winner 2 [:a :b
+                :c :a])
+;; => :a
+
+(grid-winner 2 [:a :b
+                :b :d])
+;; => :b
+
+;; row win in non-square grid
+(grid-winner 2 [:a :a
+                :b :c
+                :d :e])
+;; => :a
+
+;; col win in non-square grid
+(grid-winner 2 [:a :b
+                :a :c
+                :a :e])
+;; => :a
+
+;; ...then the winner? fn becomes:
+(defn winner?*
+  [plyr grid-width grid]
+  (= plyr (grid-winner grid-width grid)))
